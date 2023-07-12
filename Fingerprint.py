bc8caf953c3c4dae141cae840d02677f8413208f
MOD0912 <72944680+MOD0912@users.noreply.github.com>
2023-07-12 17:08:40 +0200

    Update Fingerprint.py
    Improvements, working on Coin system

--- Fingerprint.py
+++ Fingerprint.py
@@ -1,187 +1,361 @@
-import pyautogui
-import time
-import keyboard
-import numpy as np
-import cv2
+"""
+Blackjack.py
+13. June 2023
+
+Blackjack Game
+
+Author:
+Melvin Kapferer, Maximilian Koch
+"""
+from PIL import Image, ImageTk
 import customtkinter as ctk
-def pause():
-    label.config(text="Script idle")
-    root.update()
-    while True:
-        time.sleep(0.1)
-        if keyboard.is_pressed("strg+e"):
-            break
-        if keyboard.is_pressed("strg+t"):
-            exit()
-    label.config(text="Print active")
-    root.update()
-    return True
+from enum import Enum
+import random
+import sys
+import os
+import re
+import time
 
 
-def click(x, o, t, y):
-    t = list[2]
-    xstart = list[0]
-    ystart = list[1]
-    y = o - 1
-    if x == 625:
-        x = 1
-    else:
-        x = 0
-    if t == 0:
-        time.sleep(0.2)
-    t += 1
-    list[2] = t
-    if x > xstart:
-        xchange = +1
-    if x == xstart:
-        xchange = 0 
-    if x < xstart:
-        xchange = -1
-    if y > ystart:
-        ychange = y - ystart
-    if y == ystart:
-        ychange = 0
-    if y < ystart:
-        ychange = y - ystart
-    list[0] = x
-    list[1] = y
-    if xchange > 0:
-        xdircetion = "d"
-    if xchange < 0: 
-        xdircetion = "a"
-    try:
-        keyboard.press_and_release(xdircetion)
-    except:
-        pass
-    if ychange > 0:
-        ydirection = "s"
-        step = 1
-    if ychange < 0:
-        ydirection = "w"
-        step = -1
-    try:
-        for esaf in range(0, ychange, step):
-            keyboard.press_and_release(ydirection)
-    except:
-        pass
-    if keyboard.is_pressed("strg+r"):
-        quit = pause()
-        if quit:
-            return True
-    keyboard.press_and_release("enter")
-    if t == 4:
-        keyboard.press_and_release("tab")
-        list[3] = 0
-        control = 0
-        while True:
-            time.sleep(0.1)
-            checkimage = pyautogui.screenshot()
-            checkimage = np.array(checkimage)   
-            checkimage = checkimage[ycheck:ycheck+heightcheck, xcheck:xcheck+widthcheck]
-            checkimage2 = sharpen_image(checkimage)
-            for i in liist:
-                checkbox = cv2.imread(f"pictures\checkbox{i}.bmp")
-                checkbox2 = sharpen_image(checkbox)
-                similarity2 = cv2.matchTemplate(checkimage2, checkbox2, cv2.TM_CCOEFF_NORMED)
-                similarity = cv2.matchTemplate(checkimage, checkbox, cv2.TM_CCOEFF_NORMED)
-                similarity2 = abs(np.max(similarity2) * 100)
-                similarity = abs(np.max(similarity) * 100)
-                if keyboard.is_pressed("strg+r"):
-                    quit = pause()
-                    if quit:
-                        return True
-                list[3] = similarity
-                if similarity > 90:
-                    control = 1
-                if control == 1 and similarity < 30:
-                    list[0] = 0
-                    list[1] = 0
-                    list[2] = 0
-                    list[3] = 0
-                    return True
+# remove not-working auto-dpi awareness
+ctk.deactivate_automatic_dpi_awareness()
+
+# enumerators for players / winners
+class PlayerKind(Enum):
+    player = "Player"
+    croupier = "Croupier"
+
+
+class WinnerKind(Enum):
+    player = "Player"
+    croupier = "Croupier"
+    draw = "Draw"
+
+
+class Cards:
+    """
+    Cards
+    """
+    @staticmethod
+    def function() -> list[str]:
+        """
+        generates a list of all available images in ./cards/
+        """
+        path = "cards"
+        filelist = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file == "CardBack.png":
+                    continue
+                filelist.append(os.path.join(root, file))
+        return filelist
+
+
+class BlackjackGame(ctk.CTk):
+    second_card_counter: int = ...
+    player_cards_x: int = ...
+    player_cards_y: int = ...
+    croupier_cards_x: int = ...
+    croupier_cards_y: int = ...
+    x_change: int = ...
+    y_change: int = ...
+    value_player: int = ...
+    value_croupier: int = ...
+    lst: list[ImageTk.PhotoImage] = ...
+    filelist: list[str] = ...
+
+    bg: ImageTk.PhotoImage = ...
+    canvas1: ctk.CTkCanvas = ...
+    hit_btn: ctk.CTkButton = ...
+    stand_btn: ctk.CTkButton = ...
+    leave_btn: ctk.CTkButton = ...
+
+    croupier_text: int = -1
+    player_text: int = -1
+    hidden_card: int = -1
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.width = self.winfo_screenwidth()
+        self.height = self.winfo_screenheight()
+        self.playercoins = 500
+
+        # window configuration
+        # check if the os is windows, and if not set fullscreen by using
+        # root.attributes instead of zoomed (state won't work on linux / darwin
+        if sys.platform == "win32":
+            self.after(0, lambda: self.state("zoomed"))
+        else:
+            self.attributes("-zoomed", 1)
+
+        self.attributes("-fullscreen", True)
+        self.bg = ImageTk.PhotoImage(
+            Image.open("./design/design1.png").resize(
+                size=(self.width, self.height)
+            )
+        )
+        self.delet()
+        self.create_widgets()
+        self.coins()
+
+    def coins(self):
         
-
-def sharpen_image(image):
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    sharpened_image = cv2.filter2D(image, -1, kernel)
-    return sharpened_image
-
-
-def controll(im, ima, x, o, p, y):
-    gray1 = im
-    gray2 = ima
-    result = cv2.matchTemplate(gray1, gray2, cv2.TM_CCOEFF_NORMED)
-    similarity = abs(np.max(result) * 100)
-    if similarity > 50:
-        quit = click(x, o, t, y)
-        if quit:
-            return True
+        self.coinframe.pack()
+        """
+        handles the coins and bets of the game
+        """
 
 
-def movetoandclick():
-    a = 0
-    while True:
-        time.sleep(0.1)
-        zahl = 0
-        o = 0
-        y = ystart
-        image = pyautogui.screenshot()
-        image = np.array(image)
-        for i in range(0, 8):
-            if i % 2 == 0:
-                y = int(ys[o])
-                o +=1
-            if zahl == 1:
-                x = x1
-                zahl = zahl - 1
+    def delet(self) -> None:
+        """
+        resets all values to game start
+        """
+
+
+        print(self.keys())
+        self.second_card_counter = 0
+        self.player_cards_x = int(self.width / 2)
+        self.player_cards_y = int(self.height / 1.479)
+        self.croupier_cards_x = int(self.width / 2)
+        self.croupier_cards_y = int(self.height / 4.695)
+        self.x_change = 20
+        self.y_change = 20
+        self.value_player = 0
+        self.value_croupier = 0
+        self.lst = []
+        self.filelist = Cards().function()
+
+
+    def create_widgets(self) -> None:
+        """
+        creates all the game-widgets
+        """
+        self.canvas1 = ctk.CTkCanvas(self, border=0, highlightthickness=0)
+        self.canvas1.pack(fill="both", expand=True)
+        self.canvas1.create_image(0, 0, image=self.bg, anchor="nw")
+        self.croupier_text = self.canvas1.create_text(
+            self.width / 2,
+            self.height / 21.6,
+            text=f"croupier value: {self.value_croupier}",
+            font=(
+                'Times New Roman', 20,
+                'bold'
+            ),
+            fill="white"
+        )
+        self.player_text = self.canvas1.create_text(
+            self.width / 2,
+            self.height / 2,
+            text=f"croupier value: {self.value_croupier}",
+            font=(
+                'Times New Roman',
+                20,
+                'bold'
+            ),
+            fill="white"
+        )
+        self.hit_btn = ctk.CTkButton(
+            self.canvas1,
+            text="Hit",
+            height=int(self.height / 21.6),
+            width=int(self.width / 9.6),
+            command=lambda: self.create_cards(PlayerKind.player),
+            fg_color="#202020",
+            corner_radius=0
+        )
+        self.hit_btn.place(
+            x=self.width / 2 - self.width / 4.8,
+            y=self.height - self.height / 10.8
+        )
+        self.stand_btn = ctk.CTkButton(
+            self.canvas1,
+            text="Stand",
+            height=int(self.height / 21.6),
+            width=int(self.width / 9.6),
+            command=self.stand,
+            fg_color="#202020",
+            corner_radius=0
+        )
+        self.stand_btn.place(
+            x=self.width / 2,
+            y=self.height - self.height / 10.8,
+            anchor="n"
+        )
+        self.leave_btn = ctk.CTkButton(
+            self.canvas1,
+            text="Hit and run",
+            height=int(self.height / 21.6),
+            width=int(self.width / 9.6),
+            command=exit,
+            fg_color="#202020",
+            corner_radius=0
+        )
+        self.leave_btn.place(
+            x=self.width / 2 + self.width / 4.8,
+            y=self.height - self.height / 10.8,
+            anchor="ne"
+        )
+        self.coinframe = ctk.CTkFrame(self.canvas1)
+        self.create_cards(PlayerKind.player)
+        self.create_cards(PlayerKind.croupier)
+        self.create_cards(PlayerKind.player)
+        self.create_cards(PlayerKind.croupier)
+
+    def create_cards(self, player: PlayerKind) -> None:
+        """
+        creates one card and places it
+
+        :param player: must be either Player or Croupier
+        """
+        if len(self.filelist) == 0:
+            self.filelist = Cards().function()
+
+        card = random.choice(self.filelist)
+
+        self.filelist.remove(card)
+        value = int(
+            re.search(r'\d+', card).group()
+        )  # gets integer out of a string
+
+        if player == PlayerKind.player:
+            self.player_cards_x += self.x_change
+            self.player_cards_y += self.y_change
+            x = self.player_cards_x - self.x_change
+            y = self.player_cards_y - self.y_change
+            self.value_player += value
+            print(f"player value:{self.value_player}")
+
+        elif player == PlayerKind.croupier:
+            self.second_card_counter += 1
+            if self.second_card_counter == 2:
+                card = "cards/CardBack.png"
+
             else:
-                x = x2   
-                zahl = zahl + 1
-            for p in range(0, 16):
-                ima = cv2.imread(f"pictures/{p+1}.bmp") 
-                ima = cv2.resize(ima, dsize=(width, height))  
-                im = image[y:y+height, x:x+width]
-                a+=1
-                
-                if keyboard.is_pressed("strg+r"):
-                    quit = pause()
-                    if quit:
-                        continue
-                if keyboard.is_pressed("strg+t"):
-                    exit()
-                quit = controll(im, ima, x, o, p, y)
-                if quit:
-                    return
+                self.value_croupier += value
+            self.croupier_cards_x += self.x_change
+            self.croupier_cards_y += self.y_change
+            x = self.croupier_cards_x - self.x_change
+            y = self.croupier_cards_y - self.y_change
 
-root = ctk.CTk()
-root.geometry("80x20+0+0")
-root.lift()
-root.attributes("-topmost", True)
-root.wm_overrideredirect(True)
-label = ctk.CTkLabel(root, text="Script idle")
-label.pack()
-root.update()
-while not keyboard.is_pressed("strg+e"):
-    time.sleep(0.1)
-    pass
-label.config(text="Print active")
-root.update()
-while True:
-    w, h = pyautogui.size() 
-    x1 = int(w / 3.072)
-    x2 = int(w / 4.0)
-    zahl = 0
-    ystart = int(h / 3.898916967509025)
-    width = int(w / 17.94392523364486)
-    height = int(h / 10)
-    ys = [h / 3.898916967509025, h / 2.5714285714285716, h / 1.9148936170212767, h / 1.5254237288135593]
-    t = 0
-    y = ystart
-    ycheck = 475
-    xcheck = 676
-    heightcheck = 130
-    widthcheck = 568
-    list = [0, 0, 0, 0]
-    liist = ["b", "w"]
-    movetoandclick()
+            print(f"croupier value:{self.value_croupier}")
 
+        else:
+            raise ValueError("Invalid value for player!")
+
+        self.canvas1.itemconfig(
+            self.croupier_text,
+            text=f"croupier value: {self.value_croupier}"
+        )
+        self.canvas1.itemconfig(
+            self.player_text,
+            text=f"player value: {self.value_player}"
+        )
+        card = ImageTk.PhotoImage(Image.open(card).resize(
+            (int(self.width / 9.6), int(self.height / 3.6)))
+        )
+        image = self.canvas1.create_image(x, y, image=card)
+
+        if self.second_card_counter == 2 and player == "Croupier":
+            self.hidden_card = image
+        self.lst.append(card)  # handles common error
+        if self.value_player > 21:
+            self.end(WinnerKind.croupier)
+
+        elif self.value_croupier > 21:
+            self.end(WinnerKind.player)
+
+        if self.value_player == 21:
+            self.stand()
+
+    def stand(self) -> None:
+        """
+        ends the players turn
+        """
+        self.croupier_cards_x = self.croupier_cards_x - self.x_change
+        self.croupier_cards_y = self.croupier_cards_y - self.y_change
+        self.canvas1.delete(self.hidden_card)
+
+        while self.value_croupier < 17:
+            self.create_cards(PlayerKind.croupier)
+            time.sleep(0.5)
+
+        if self.value_croupier < self.value_player:
+            winner = WinnerKind.player
+
+        elif self.value_croupier > self.value_player:
+            winner = WinnerKind.croupier
+
+        else:
+            winner = WinnerKind.draw
+
+        if self.value_croupier > 21 or self.value_player > 21:
+            return
+
+        self.end(winner)
+
+    def end(self, winner: WinnerKind) -> None:
+        """
+        handles the end of a round
+
+        :param winner: the rounds winner
+        """
+        print(f"winner is {winner}")
+        if winner == WinnerKind.draw:
+            text = "draw"
+            x = 625
+
+        else:
+            text = f"{winner.value} won!"
+
+            if winner == WinnerKind.player:
+                x = 200
+            elif winner == WinnerKind.croupier:
+                x = 125
+            else:
+                raise ValueError("Invalid value for winner")
+
+        self.hit_btn.configure(state="disabled")
+        self.stand_btn.configure(state="disabled")
+        self.frame_end = ctk.CTkFrame(self, width=500, height=500)
+        self.frame_end.place(x=x, y=300)
+        label_end = ctk.CTkLabel(
+            self.frame_end,
+            text=text,
+            font=('Times New Roman', 300, 'bold')
+        )
+        label_end.pack()
+        button_end_restart = ctk.CTkButton(
+            self.frame_end,
+            text="Again?",
+            command=self.close,
+            height=50
+        )
+        button_end_restart.pack(fill="x")
+        button_end_end = ctk.CTkButton(
+            self.frame_end,
+            text="End the game",
+            command=exit,
+            height=50
+        )
+        button_end_end.pack(fill="x")
+
+    def close(self) -> None:
+        """
+        restarts the game
+        """
+        self.hit_btn.configure(state="normal")
+        self.stand_btn.configure(state="normal")
+        self.frame_end.destroy()
+        self.lst.clear()
+        print(self.lst)
+        self.delet()
+        time.sleep(0.1)
+        self.create_cards(PlayerKind.player)
+        self.create_cards(PlayerKind.croupier)
+        self.create_cards(PlayerKind.player)
+        self.create_cards(PlayerKind.croupier)
+
+
+if __name__ == '__main__':
+    game = BlackjackGame()
+    game.mainloop()
